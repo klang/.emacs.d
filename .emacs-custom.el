8743bedd2aed5d4995f376ee1eeeb3bcb5f738d7
Karsten Lang Pedersen <karsten@dashsoft.dk>
2018-11-20 19:39:37 +0100

    maybe this works

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,10 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8281e0b124c30984d03cf42ed894ad39a822439c33813d4db999aae4152b3d8d" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default))))
+ '(custom-safe-themes
+   (quote
+    ("8281e0b124c30984d03cf42ed894ad39a822439c33813d4db999aae4152b3d8d" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
+ '(jedi:key-complete nil)
+ '(org-babel-load-languages
+   (quote
+    ((emacs-lisp . t)
+     (python . t)
+     (clojure . t)
+     (sh . t)
+     (perl . t)
+     (screen . t))))
+ '(package-selected-packages
+   (quote
+    (ac-nrepl zenburn-theme yasnippet yaml-mode xml-rpc web-mode undo-tree sql-indent solarized-theme rect-mark rainbow-mode python-mode php-mode paredit json-mode jira idle-highlight hl-sexp highline highlight-symbol highlight-parentheses git-gutter exec-path-from-shell etags-select epc crosshairs col-highlight ansible-doc ac-cider-compliment ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-gutter:unchanged ((t (:background "gray21"))))
  '(slime-highlight-edits-face ((((class color) (background dark)) (:background "#202020"))) t))
